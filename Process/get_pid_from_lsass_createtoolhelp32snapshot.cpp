89af5fc8801568247784a8f516e7bc21a695621d
Huy <63166600+DebugPrivilege@users.noreply.github.com>
2023-01-12 19:49:40 +0100

    Update get_pid_from_lsass_createtoolhelp32snapshot.cpp

--- Process/get_pid_from_lsass_createtoolhelp32snapshot.cpp
+++ Process/get_pid_from_lsass_createtoolhelp32snapshot.cpp
@@ -1,63 +1,54 @@
 #include <iostream>
 #include <windows.h>
 #include <TlHelp32.h>
+#include <sstream>
 
-struct ProcessInfo
-{
-    DWORD processPID = 0;
-    std::wstring processName;
+struct ProcessInfo {
+	DWORD processPID = 0;
+	std::wstring processName;
 };
 
-ProcessInfo getProcessInfo(const std::string& processName)
+ProcessInfo getProcessInfo()
 {
-    HANDLE snap_handler = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (snap_handler == INVALID_HANDLE_VALUE)
-    {
-        DWORD error = GetLastError();
-        std::cerr << "Error creating process snapshot: " << error << std::endl;
-        exit(1);
-    }
-
-    PROCESSENTRY32 processEntry = { sizeof(PROCESSENTRY32) };
-    std::wstring processNameWide(processName.begin(), processName.end());
-    const wchar_t* szName = processNameWide.c_str();
-
-    ProcessInfo info;
+	HANDLE snap_handler = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32 processEntry = {};
+	processEntry.dwSize = sizeof(PROCESSENTRY32);
+	std::string lsass_processname = "lsass.exe";
+	std::wstring processname(lsass_processname.begin(), lsass_processname.end());
+	const wchar_t* szName = processname.c_str();
+	ProcessInfo info;
     if (Process32First(snap_handler, &processEntry))
     {
-        do
+        while (_wcsicmp(processEntry.szExeFile, szName) != 0)
         {
-            if (_wcsicmp(processEntry.szExeFile, szName) == 0)
+            if (!Process32Next(snap_handler, &processEntry))
             {
-                info.processPID = processEntry.th32ProcessID;
-                info.processName = processEntry.szExeFile;
                 break;
             }
-        } while (Process32Next(snap_handler, &processEntry));
+        }
+        if (_wcsicmp(processEntry.szExeFile, szName) == 0)
+        {
+            info.processPID = processEntry.th32ProcessID;
+            info.processName = processEntry.szExeFile;
+        }
     }
-
-    // Check for errors
-    if (info.processPID == 0)
+    // Check for errors if (info.processPID == 0)
     {
         DWORD error = GetLastError();
         if (error != ERROR_SUCCESS)
         {
-            std::cerr << "Error getting process information: " << error << std::endl;
-            exit(1);
+            std::cerr << "Error getting process PID: " << error << std::endl;
         }
     }
-
     CloseHandle(snap_handler);
-    processNameWide.clear();
-
+    processname.clear();
     return info;
 }
-
 int main(int argc, char** argv)
 {
-    const std::string processName = "lsass.exe";
-    ProcessInfo info = getProcessInfo(processName);
-
+    // Obtains LSASS Process info
+    ProcessInfo info = getProcessInfo();
     std::cout << "PID: " << info.processPID << std::endl;
+    // Print the PID and name to the console
     std::wcout << "ProcessName: " << info.processName << std::endl;
 }
